0d75ef2e7f280eb8636e46a6d341526d010d4900
David DiGioia <davidofjoy@gmail.com>
2020-04-26 09:44:09 -0400

    init commit

---
+++ src/websites.py
@@ -0,0 +1,14 @@
+
+class Target:
+    def __init__(self, url, online):
+        self.url = url
+        self.online = online
+
+    def in_stock(self):
+        pass
+
+    def add_to_cart(self):
+        pass
+
+    def checkout(self):
+        pass
