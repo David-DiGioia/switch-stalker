21511f79b33378737f97b051e75621b7348ea2a2
David DiGioia <davidofjoy@gmail.com>
2020-04-27 01:20:18 -0400

    setup to purchase switches

--- src/app.py
+++ src/app.py
@@ -10,25 +10,20 @@ target_grey_url = 'https://www.target.com/p/nintendo-switch-with-gray-joy-con/-/
 # Debug
 target_url_in_stock = "https://www.target.com/p/super-mario-maker-2-nintendo-switch/-/A-54498967"
 
-quinn_url_1 = 'https://www.target.com/p/ella-mila-nail-care-nail-strengthener--first-aid-kiss----0-45-fl-oz/-/A-52901450'
-
 def main():
     # Get profile information
     options.read_profile()
     if options.run_tests and options.debug:
         tests.run_tests()
 
-    # target_neon = websites.Target(target_neon_url)
-    # task1 = task.Task(target_neon)
-    # task1.start()
-    #
-    # target_grey = websites.Target(target_grey_url)
-    # task2 = task.Task(target_grey)
-    # task2.start()
+    target_neon = websites.Target(target_neon_url)
+    task1 = task.Task(target_neon)
+    task1.start()
 
-    target_nail = websites.Target(quinn_url_1)
-    task3 = task.Task(target_nail)
-    task3.start()
+    target_grey = websites.Target(target_grey_url)
+    task2 = task.Task(target_grey)
+    task2.start()
+
 
 if __name__ == "__main__":
     main()
